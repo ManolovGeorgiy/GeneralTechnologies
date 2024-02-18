5771af914290dea14065cb50914442b7fb1f0714
Georgiy <g.manolov@web.de>
2024-02-18 16:19:54 +0100

    create home work 10

--- GeneralTechnologies/src/HomeWork/HomeWork9.js
+++ GeneralTechnologies/src/HomeWork/HomeWork9.js
@@ -1,41 +1,41 @@
-// 1. Вывести незаблокированных юзеров из Germany и USA
+1.Вывести незаблокированных юзеров из Germany и USA:
 
-db.getCollection("users").find({country: 'Germany' },
-{ country: 'USA' },
-    { is_blocked: { $ne: true }})
+db.users.find({
+    $and: [
+    {is_blocked: {$ne: true} },  // пользователь не заблоkирован
+    {country: {$in: ["Germany", "USA"] } } // пользователm из Germany и USA
+    ]
+})
 
 
-// 2. Вывести названия треков продолжительностью от 5 до 15 мин
+2. Вывести названия треков продолжительностью от 5 до 15 мин:
 
 db.tracks.find(
-    {},
-    { title: 1,duration_secs: 1, _id: 0 },
-    {duration_secs: { $gte: 5 * 60, $lte: 15 * 60 } }
-)
-
-// 3. Разблокировать юзеров не из Germany
-
-db.users.updateMany(
-    { country: 'Germany' },
     {
-        $set: {
-            is_blocked: {$ne: true}
-        }
+        duration_secs: { $gte: 5 * 60, $lte: 15 * 60 }
     }
 )
 
-// 4. Увеличить баланс назаблокированных юзеров не из Germany на 150 EUR
+
+3.Разблокировать юзеров не из Germany:
 
 db.users.updateMany(
-    { country:  { $ne: 'Germany'} },
-    {
-        $inc: {
-            balance: 150
-        }
-    }
+    { country: { $ne: "Germany" } }, // фильтр: пользователи не из Germany
+    { $set: { is_blocked: false } } // операция: установить поле is_blocked в false
 )
 
-// 5. Опишите схему БД "соц/сеть" (юзеры, публикации, друзья, чаты)
+
+4.Увеличить баланс назаблокированных юзеров не из Germany на 150 EUR:
+
+db.users.updateMany(
+    {
+        is_blocked: { $ne: true }, // фильтр: пользователи не заблокированы
+        country: { $ne: "Germany" } // фильтр: пользователи не из Germany
+    },
+    { $inc: { balance: 150 } } // операция: увеличить баланс на 150 EUR
+)
+
+5. Опишите схему БД "соц/сеть" (юзеры, публикации, друзья, чаты)
 
 Table "users":
 user_id (User identifier, primary key)
