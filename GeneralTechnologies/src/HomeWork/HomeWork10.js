80fa181b408cd3d24fe9669f20044d086ebea36e
Georgiy <g.manolov@web.de>
2024-02-18 16:00:08 +0100

    create home work 10

--- GeneralTechnologies/src/HomeWork/HomeWork10.js
+++ GeneralTechnologies/src/HomeWork/HomeWork10.js
@@ -1,14 +1,63 @@
+1. Завершить с помощью инструмента онлайн-проектирования создание схемы БД "платежная система" (юзеры, счета, транзакции)
 
+payment system
+
+https://dbdiagram.io/d/65d200e6ac844320ae66c4dc
+
+Table Users {
+user_id int
+username varchar
+email varchar
+password varchar
+fullname varchar
+lastname varchar
+birthdate date
+location varchar
+
+}
+Table Accounts {
+account_id int
+user_id int
+account_number varchar
+balance int
+}
+
+Table Transactions {
+transaction_id int
+sender_account_id int
+receiver_account_id int
+transaction timestamp
+}
+
+Table Locks {
+lock_id int
+user_id int
+account_id int
+lock timestamp
+}
+
+Ref: "Transactions"."transaction_id" < "Users"."user_id"
+
+Ref: "Users"."user_id" < "Accounts"."account_id"
+
+Ref: "Accounts"."account_id" < "Locks"."lock_id"
+
+Ref: "Accounts"."account_id" < "Transactions"."transaction_id"
+
+
+2. Используя метод aggregate(), в рамках БД music вывести названия трех самых продолжительных треков
 
 db.tracks.aggregate([
-    { $sort: { duration: -1 } }, // Сортировка по убыванию продолжительности
-    { $limit: 3 }, // Ограничение вывода тремя результатами
-    { $project: { _id: 0, title: 1, duration: 1 } } // Выбор только нужных полей (название и продолжительность)
+    { $sort: { duration_secs: -1 } },
+    { $limit: 3 }, //
+    { $project: { _id: 0, title: 1, duration_secs: 1 } }
 ])
 
+3. Используя метод aggregate(), в рамках БД music вывести имя одного случайного юзера не из USA
+
 db.users.aggregate([
-    { $match: { country: { $ne: "USA" } } }, // Фильтрация пользователей, не из США
-    { $sample: { size: 1 } }, // Выбор одного случайного пользователя
-    { $project: { _id: 1, name: 1 } } // Выбор только имени пользователя
+    { $match: { country: { $ne: "USA" } } },
+    { $sample: { size: 1 } },
+    { $project: { _id: 1, name: 1 } }
 ])
 
